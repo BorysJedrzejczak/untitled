e119cf4eff2cf1d346b626af6ef883fa710ddb15
BorysJedrzejczak <241205@edu.p.lodz.pl>
2022-10-02 17:24:34 +0200

    a

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    return 0;
+
+    char c;
+
+    cout << "wpisz c zeby zrobic salto\n";
+    cin >> c;
+
+    if (c == 'c'){
+        cout << "saltooooo";
+        return 0;
+    }
+
+    else{
+        cout << "nie ma salta";
+        return 1;
+    }
 }
